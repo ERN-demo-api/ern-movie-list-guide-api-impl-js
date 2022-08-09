b0573d8c2a447b3ee5cf2d5845163479be47a70e
JoeSuzuki <joesuzuki516@gmail.com>
2022-08-09 02:16:08 -0700

    Add updated implementation

---
+++ README.md
@@ -0,0 +1,9 @@
+# ern-movie-list-guide-api-impl-js
+
+A demo Electrode Native API JS implementation returning some movies, because the original tutorial repo no longer works. 🙌
+
+## Installation
+
+```bash
+$ yarn add ern-movie-list-guide-api-impl-js
+``` 
---
+++ js/MoviesApiImpl.js
@@ -0,0 +1,25 @@
+import { MoviesApi } from 'ern-movie-list-demo-api'
+
+MoviesApi.requests().registerGetTopRatedMoviesRequestHandler((requestData) => {
+  let movies = [
+    {
+      title: 'TitanicJS',
+      releaseYear: 1997,
+      ratings: '4.5',
+      imageUrl: 'http://bit.ly/2hnU8mq',
+      description: 'Titanic',
+    }, {
+      title: 'AvatarJS',
+      releaseYear: 2009,
+      ratings: '4.0',
+      imageUrl: 'http://bit.ly/2xAX0Cv',
+      description: 'Avatar',
+    },
+  ]
+  return Promise.resolve(movies)
+})
+
+MoviesApi.requests().registerGetMovieDetailRequestHandler((requestData) => {
+  //TODO: Write your implementation and resolve or reject the promise
+  //return Promise.resolve(result) || Promise.reject(err)
+})
